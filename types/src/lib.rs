e035f448cfe55d285dd88cc22e4bfe505ebcd511
daltoncoder <71679972+daltoncoder@users.noreply.github.com>
2025-12-01 11:09:12 -0500

    add endpoint to retreieve deposit transaction (#67)

--- types/src/lib.rs
+++ types/src/lib.rs
@@ -29,6 +29,8 @@ use commonware_consensus::simplex::types::Activity as CActivity;
 pub type Digest = commonware_cryptography::sha256::Digest;
 pub type Activity = CActivity<Signature, Digest>;
 
+pub const PROTOCOL_VERSION: u32 = 1;
+
 /// Auxiliary data needed for block construction
 #[derive(Debug, Clone)]
 pub struct BlockAuxData {
